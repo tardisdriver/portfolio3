4ccd02837ccec475e2b59b67ec82601048bd968a
Tracy Stark <stark.tracy@gmail.com>
2018-08-16 20:06:14 -0500

    super secret console stuff

--- UI.js
+++ UI.js
@@ -12,3 +12,12 @@ $(document).ready(function() {
          });
     });
 });
+
+let o = "font-family: Helvetica, Arial, sans-serif", 
+r = o + "; font-size:32px; line-height: 1.5; color: #fff",
+a = r + "; background-color: #8000ff; padding: 4px 220px 4px 0",
+l = o + "; font-size:14px; line-height: 1.15rem",
+c = l + "; text-decoration: underline",
+u = "";
+
+window.console.log("\n%cHello There", a);
